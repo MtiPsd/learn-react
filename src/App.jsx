973da298862646bf43f7beb3843446f516e19de6
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-08-12 19:59:05 +0330

    start creating tic tac toe with what we learned

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import CustomHook from './learn/custom_hooks';
 import HookFlow from './learn/hook_flow';
 import LiftingState from './learn/lifting_state';
+import Game from './learn/tic_tac_toe';
 /////////////////////////////
 /////////////////////////////
 /////////////////////////////
 function App() {
   return (
     <>
-      <LiftingState />
+      <Game />
     </>
   );
 }
